38c3a3020347e3c19f1ad2c54e47cd5631cefa9a
Raul Lopez <raul.lopez@oriflame.com.gt>
2022-10-13 09:42:26 -0600

    Se agregan las capas de seguridad

---
+++ Ventas_BE/Reportes_BE.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ventas_BE
+{
+    public class Reportes_BE
+    {
+    }
+}
