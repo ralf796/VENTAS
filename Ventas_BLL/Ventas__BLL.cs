5cca39d5e8383ab7d7e7feabbd80ed7c4a24c9da
Raul Lopez <raul.lopez@oriflame.com.gt>
2022-10-26 18:11:06 -0600

    Se avanza la pantalla de ventas

---
+++ Ventas_BLL/Ventas__BLL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas_BE;
+using Ventas_DAL;
+
+namespace Ventas_BLL
+{
+    public class Ventas__BLL:IDisposable
+    {
+        public void Dispose() { }
+        public static List<Ventas__BE> GetDatosSP(Ventas__BE item)
+        {
+            List<Ventas__BE> data = null;
+            using (var model = new Ventas__DAL())
+            {
+                data = model.GetDatosSP(item);
+            }
+            return data;
+        }
+    }
+}
---
+++ Ventas_DAL/Ventas__DAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ventas_BE;
+
+namespace Ventas_DAL
+{
+    public class Ventas__DAL : IDisposable
+    {
+        public void Dispose() { }
+        public List<Ventas__BE> GetDatosSP(Ventas__BE item)
+        {
+            List<Ventas__BE> result = new List<Ventas__BE>();
+            using (var model = new Base_SQL("sp_ventas"))
+            {
+                model.Command.Parameters.AddWithValue("@MTIPO", item.MTIPO);
+                model.Command.Parameters.AddWithValue("@NIT", item.NIT);
+                result = model.GetData<Ventas__BE>();
+            }
+            return result;
+        }
+    }
+}
